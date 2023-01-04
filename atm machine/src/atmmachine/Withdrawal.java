9d24a8aef6cc9cc1c66c2b539c6c9b0c7d08da43
hamzat <ayomidelegbede@gmail.com>
2023-01-04 19:41:42 +0100

    added a new line

--- atm machine/src/atmmachine/Withdrawal.java
+++ atm machine/src/atmmachine/Withdrawal.java
@@ -2,7 +2,28 @@ package atmmachine;
 
 public interface Withdrawal {
     int withdrawal();
-    public int amountWithdrawn{
-        return get  
+    public static int withdrawal(int balance,
+                                        int withdrawAmount)
+    {
+        System.out.println("Withdrawn Operation:");
+        System.out.println("Withdrawing Amount : "
+                + withdrawAmount);
+        if (balance >= withdrawAmount) {
+            balance = balance - withdrawAmount;
+            System.out.println(
+                    "Please collect your money and collect the card");
+            getbalance()= depositbalance();
+        }
+        else {
+            System.out.println("Sorry! Insufficient Funds");
+            System.out.println();
+        }
+        return balance;
     }
+
+    static int getbalance() {
+        return getbalance();
+    }
+
 }
+
