8ab6816e7cc0d5af4a5704fa9f29a8ce95a1486a
hamzat <ayomidelegbede@gmail.com>
2023-01-06 08:00:21 +0100

    improvement on the phone number prompt in the buy airtime feature

--- atm machine/src/atmmachine/BuyAirtimeAction.java
+++ atm machine/src/atmmachine/BuyAirtimeAction.java
@@ -12,13 +12,9 @@ public class BuyAirtimeAction implements ATMAction {
     public AtmActionParameter preAction() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter amount to be bought :");
-        int buyAirtime = input.nextInt();
+        int buyAirtimeAmount = input.nextInt();
         AtmActionParameter actionParameter = new AtmActionParameter();
-        actionParameter.setBuyairtime(buyAirtime);
-        Scanner inputPhoneNumber = new Scanner(System.in);
-        System.out.println("Enter phone number for the airtime:");
-        String phoneNumber = inputPhoneNumber.next();
-        int intPhoneNumber = Integer.parseInt(phoneNumber);
+        actionParameter.setBuyairtime(buyAirtimeAmount);
         return actionParameter;
 
 
@@ -27,15 +23,22 @@ public class BuyAirtimeAction implements ATMAction {
     @Override
     public void action(AtmActionParameter atmActionParameter) {
         int buyAirtime = atmActionParameter.getBuyairtime();
-
-
+        Scanner inputPhoneNumber = new Scanner(System.in);
+        System.out.println("Enter phone number for the airtime:");
+        String phoneNumber = inputPhoneNumber.next();
+        int intPhoneNumber = Integer.parseInt(phoneNumber);
+        if (phoneNumber.length()== 11){
             if (buyAirtime <= BankAccount.BANK_ACCOUNT_BALANCE) {
                 BankAccount.BANK_ACCOUNT_BALANCE = BankAccount.BANK_ACCOUNT_BALANCE - buyAirtime;
-                System.out.println("Airtime purchase successful");
+                System.out.println("Airtime purchase successful for this number " + phoneNumber );
 
             } else {
                 System.err.println("insufficient balance");
             }
+        }else {
+            System.err.println("Incorrect number format");
+        }
+
         }
 
     }
